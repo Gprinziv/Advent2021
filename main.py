797fdaee4696e7e6a5264417544579ff9bee2732
Gprinziv <gprinziv@gmail.com>
2021-12-02 14:45:08 +0900

    Day 2 is a wrap!

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-with open("depths") as file:
-    depths = [int(line.strip()) for line in file.readlines()]
+with open("instructions") as file:
+  moves = [line.strip().split(" ") for line in file.readlines()]
 
 #region Part 1
-total = 0
-cur = depths[0]
-for i in range(1, len(depths)):
-    if depths[i] > cur:
-        total += 1
-    cur = depths[i]
-print(total)
 #endregion
 
-#region part 2
-total = 0
-cur = [depths[0], depths[1], depths[2]]
-for i in range(3, len(depths)):
-    temp = cur[1:] + [depths[i]]
-    if sum(temp) > sum(cur):
-        total += 1
-    cur = temp
-print(total)
+#region Part 2
 #endregion
