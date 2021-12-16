bd2424443eb92fe90ff1bf1a1f9b25cbfac7c505
Giovanni Prinzivalli <Gprinziv@gmail.com>
2021-12-16 21:56:07 +0900

    16 is WIP

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
+def literal(packet):
+  cur, number = 0, ""
+  while packet[cur] == '1':
+    number += packet[cur+1:cur+5]
+    cur += 5
+  number += packet[cur+1:cur+5]
+  print("Literal contents: " + number)
+  print("As an integer: " + str(int(number, 2)))
+  return number
+
+def operator(packet):
+  return
+
+def idpacket(packet):
+  return
+
 with open("test") as file:
-  raw = file.readlines()
+  bits = bin(int(file.read(), 16))[2:]
+
+toProcess = [bits]
+versions = []
+
+while toProcess:
+  packet = toProcess.pop(0)
+  ver = int(packet[:3], 2)
+  versions.append(vid)
+  tid = int(packet[3:6], 2)
+  lid = packet[6]
+  print(tid)
+  if tid == 4:
+    literal(packet[6:])
+  elif lid == '1':
+    pass
+  else:
+    pass
