504298636c025f64c3f4a8d380ba321c3ea1e920
Gprinziv <gprinziv@gmail.com>
2021-12-16 14:56:11 +0900

    Day 16 in progress

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 with open("test") as file:
-  raw = file.read().split("\n\n")
+  bits = bin(int(file.read(), 16))[2:]
+
+ver = int(bits[:3], 2)
+tid = int(bits[3:6], 2)
+lid = bits[7]
+
+if tid == 4:
+  print("Yay!")
+else:
+  pass
+
+print(ver)
+print(pid)
